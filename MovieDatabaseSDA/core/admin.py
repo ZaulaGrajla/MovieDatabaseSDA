3027d39c0dbaac407a24e702eeb9ae547a1f848e
Paulina Grajewska <paulina_darcy@o2.pl>
2020-09-06 11:47:15 +0200

    Add model Genre

--- MovieDatabaseSDA/core/admin.py
+++ MovieDatabaseSDA/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from core.models import Movie, Genre
 
-# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
--- MovieDatabaseSDA/core/models.py
+++ MovieDatabaseSDA/core/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age_limit = models.IntegerField(
+        choices=[(7, 'little kid'), (13, 'teen'), (18, 'EU adult'), (21, 'US adult'), (36, 'senator')],
+        blank=True,
+        null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField(
@@ -11,6 +22,7 @@ class Movie(models.Model):
     description = models.TextField(null=True, blank=True)
     # null do baz danych a blank do formularzy
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.SET_NULL, blank=True)
 
     def __str__(self):
         return f"{self.title} from {self.released}"
